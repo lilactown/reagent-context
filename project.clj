6f9090dd789465035a8be6fe98b001a8d711c44f
Will Acton <will@willacton.com>
2018-04-29 11:40:22 -0700

    bump version to show license et. al. on clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 ;; this project.clj is only used for releases.
 ;; shadow-cljs.edn should be treated as the source of truth
 ;; for building & testing
-(defproject lilactown/reagent-context "0.0.1"
+(defproject lilactown/reagent-context "0.0.2"
+  :license {:name "MIT"}
+  :description "Easy access to React context in ClojureScript & Reagent"
+  :url "https://github.com/Lokeh/reagent-context"
   :dependencies [[reagent "0.8.0"]]
   :source-paths ["src"])
