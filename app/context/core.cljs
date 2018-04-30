51069f6c310d72d7e6618cf9616312a9306fdad5
Will Acton <will@willacton.com>
2018-04-30 14:25:38 -0700

    defconsumer creates a ratom and binds it within body

--- app/context/core.cljs
+++ app/context/core.cljs
@@ -9,9 +9,10 @@
 (defconsumer
   testing
   my-context
-  [{:keys [color toggle testing]} & children]
-  (js/console.log testing)
-  (into [:div {:style {:background-color color}}] children))
+  [state & children]
+  ;; (js/console.log state)
+  (let [{:keys [color toggle testing]} @state]
+    (into [:div {:style {:background-color color}}] children)))
 
 (defn toggle-color [{:keys [color] :as state}]
   (merge state (if (= color "red") {:color "blue"} {:color "red"})))
@@ -29,33 +30,61 @@
         (fn [{:keys [color toggle]}]
           [:div
            [:div color]
-           [:button {:on-click toggle} "color"]])]
-       [:h1 "hi"]
-       [:h2 "hey"]]))
+           [:button {:on-click toggle} "color"]])]]))
   )
 
 (defn toggle-testing [testing]
   (js/console.log testing)
   (if (= testing "green") "light-blue" "green"))
 
+;; (defn magic-consumer [& children]
+;;   (let [context-state (r/atom nil)]
+;;     (fn [& children]
+;;       (into
+;;        [:<>
+;;         [context/consumer {:context my-context}
+;;          (fn [v] (reset! context-state v)
+;;            nil)]
+;;         [:div "Current color:" @context-state]]
+;;        children))))
+
 (defn app-2 []
-  (let [state (r/atom 0)
-        other-state (r/atom "green")
-        click! #(reset! other-state "red")
-        click-inc! #(swap! state inc)]
+  (let [color (r/atom "green")
+        counter (r/atom 0)
+        click-color! #(swap! color toggle-testing)
+        click-inc! #(swap! counter inc)]
     (fn []
-      (js/console.log @other-state)
-      [context/provider
-       {:context my-context :value {:testing other-state}}
-       [testing
-        [:div
-         [:div "state: " @state
-          [:button {:on-click click-inc!} "+"]
-          [:button {:on-click click!} "color"]]]]])))
+      ;; (js/console.log color)
+      [context/provider {:context my-context :value @color}
+       [context/consumer {:context my-context}
+        (fn [color']
+          [:div "Current (non-magic) color: " color'
+           [:div "Count (non-magic): " @counter]])]
+       [:button {:on-click click-color!} "color"]
+       [:button {:on-click click-inc!} "increment"]])))
+
+(defconsumer magic-consumer my-context
+  [color counter]
+  [:div
+   [:div "Color: " @color]
+   [:div "Counter: " @counter]])
+
+(defn app-3 []
+  (let [color (r/atom "green")
+        counter (r/atom 0)
+        click-color! #(swap! color toggle-testing)
+        click-inc! #(swap! counter inc)]
+    (fn []
+      ;; (js/console.log color)
+      [context/provider {:context my-context :value @color}
+       [:div [magic-consumer counter]]
+       [:button {:on-click click-color!} "color"]
+       [:button {:on-click click-inc!} "increment"]])))
 
 (defn start []
   (r/render [page] (.getElementById js/document "app"))
-  (r/render [app-2] (.getElementById js/document "app2")))
+  (r/render [app-2] (.getElementById js/document "app2"))
+  (r/render [app-3] (.getElementById js/document "app3")))
 
 
 (defn ^:export init [] (start))
